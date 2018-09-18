84cfe351bcfdbff34bf67493442d30eca99c414e
Andrei Zgirvaci <zgirvaciandrei@gmail.com>
2018-09-18 17:49:08 +0200

    chat screen

--- src/screens/App/Chat/index.js
+++ src/screens/App/Chat/index.js
@@ -1,13 +1,40 @@
 import React from "react";
 import { View, Text } from "react-native";
+import ChatBot from "react-native-chatbot";
 
-const Chat = () => {
+const triggerAlert = () => {
+  alert("Hello Arminda");
+  return "3"
+};
+
+function Chat() {
+  const steps = [
+    {
+      id: "1",
+      message: "What is your name?",
+      trigger: "2"
+    },
+    {
+      id: "2",
+      user: true,
+      trigger: triggerAlert
+    },
+    {
+      id: "3",
+      message: "Hello Max",
+      end: true
+    },
+  ];
+
   return (
     <View>
-      <Text>Hello Arminda!!!</Text>
-      <Text>Hello Mihai!!!</Text>
+      <ChatBot
+        style={{ height: "70%" }}
+        steps={steps}
+        botAvatar={"https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"}
+      />
     </View>
   );
-};
+}
 
 export default Chat;
