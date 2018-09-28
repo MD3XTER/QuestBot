fc570405cbe4306726056e69d53b350ab0ee7b59
Andrei Zgirvaci <zgirvaciandrei@gmail.com>
2018-09-28 09:55:28 +0200

    fix api

--- src/screens/App/Greeting/styles.js
+++ src/screens/App/Greeting/styles.js
@@ -16,12 +16,12 @@ export default styles = StyleSheet.create({
   continueButton: {
     flex: 1,
     backgroundColor: 'black',
-    marginTop:10,
-    paddingTop:15,
-    paddingBottom:15,
-    marginLeft:30,
-    marginRight:30,
-    borderRadius:10,
+    marginTop: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
+    marginLeft: 30,
+    marginRight: 30,
+    borderRadius: 10,
     borderWidth: 1,
   }
 });
