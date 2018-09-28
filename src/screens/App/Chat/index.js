cb21a464752b1db1eb5cde1422b198fc97b14882
Andrei Zgirvaci <zgirvaciandrei@gmail.com>
2018-09-28 09:47:03 +0200

    fix api

--- src/screens/App/Chat/index.js
+++ src/screens/App/Chat/index.js
@@ -4,28 +4,27 @@ import { connect } from "react-redux";
 import ChatBot from "react-native-chatbot";
 
 import styles from "./styles";
-import { getSteps, getStepsFromQuestionnaire } from "../../../utils"
+import { getSteps, getStepsFromQuestionnaire } from "../../../utils";
 
 class Chat extends Component {
   render () {
-    getStepsFromQuestionnaire();
+    const steps = getStepsFromQuestionnaire();
+
     return (
       <View>
         <ChatBot
-          steps={getSteps()}
+          steps={steps}
           botAvatar={
             "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"
           }
         />
-      <View style={styles.container}>
-        <ChatBot steps={}/>
       </View>
     );
   }
 }
 
 const mapStateToProps = ({ app }) => {
-  return { steps: app.steps };
+  return { steps: app.steps }
 };
 
 export default connect(
