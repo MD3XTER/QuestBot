8b1457398107571390c499f5480b9ba0a45ecee8
Andrei Zgirvaci <zgirvaciandrei@gmail.com>
2018-10-04 19:13:28 +0200

    fix api

--- src/screens/App/Chat/index.js
+++ src/screens/App/Chat/index.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import { View } from "react-native";
 import { connect } from "react-redux";
 import ChatBot from "react-native-chatbot";
 
 import styles from "./styles";
-import { getSteps, getStepsFromQuestionnaire } from "../../../utils";
+import { getSteps, getStepsFromQuestionnaire, navigateTo } from "../../../utils";
 
 class Chat extends Component {
   state = {
@@ -12,24 +11,21 @@ class Chat extends Component {
   };
 
   componentWillMount() {
-    let steps = [
-      {
-        id: "1",
-        message: "test",
-        end: true
-      }
-    ];
-
-    steps = getStepsFromQuestionnaire();
+    const steps = getStepsFromQuestionnaire();
 
     this.setState({ steps });
   }
+
+  handleEnd = () => {
+    setTimeout(() => navigateTo("ThankYou"), 2000);
+  };
 
   render () {
     const { steps } = this.state;
 
     return (
       <ChatBot
+        handleEnd={this.handleEnd}
         steps={steps}
         botAvatar={
           "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"
